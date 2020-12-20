f4a013086c3e8018376539d0837d6ea506d851a8
vikas <vikas32567@gmail.com>
2020-12-20 16:07:21 +0530

    configure Swashbuckle

--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -1,8 +1,13 @@
+#region For_Custom_Swagger_UI
 using System;
+using System.Reflection;
+using System.IO;
+using Microsoft.OpenApi.Models;
+#endregion
+
 using WebApp.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +31,31 @@ namespace WebApp
         {
             services.AddControllersWithViews();
             services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDbContext<CloudBarEfContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("SqlDatabase")));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "CloudBar",
+                    Description = "Order drinks online.",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Vikas Tadala",
+                        Email = "vikas21193@gmail.com",
+                    }
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
             // Register Repos
             services.AddTransient<IDapperDrinkRepo, DapperDrinkRepo>();
             services.AddTransient<IDapperSpiritRepo, DapperSpiritRepo>();
@@ -55,6 +85,17 @@ namespace WebApp
             app.UseRouting();
 
             app.UseAuthorization();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.InjectStylesheet("/css/site.css");
+            });
 
             app.UseEndpoints(endpoints =>
             {
