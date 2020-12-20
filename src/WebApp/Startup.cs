bbc55a4b50d280393396d8ccefdccfe053b654ae
vikas <vikas32567@gmail.com>
2020-12-20 08:38:43 +0530

    Add Database exception page

--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -24,6 +24,7 @@ namespace WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDatabaseDeveloperPageExceptionFilter();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
